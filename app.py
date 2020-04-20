aa5b373825b3ead4fef05f19ea68060de4ad5c4e
aydin <aydinjalil@gmail.com>
2020-04-20 01:04:40 -0400

    some crazy shit. I have to figure out what I did

--- app.py
+++ app.py
@@ -1,22 +1,47 @@
 from flask import Flask, render_template, jsonify, redirect, request
+import json
+from pymongo import MongoClient
 from flask_pymongo import PyMongo
+from bson import json_util
 from bson.json_util import dumps
+
 # from random import sample
 
 app = Flask(__name__)
-app.config['MONGO_URI'] = "mongodb://localhost:27017/stack_overflow"
-mongo = PyMongo(app)
+MONGODB_HOST = 'localhost'
+MONGODB_PORT = 27017
+DATABASE_NAME = 'stack_overflow'
+COLLECTION_NAME = 'developers_cleaned'
+FIELDS = {'MainBranch': True, 'Hobbyist': True, 'Employment': True, 'Country': True, 'EdLevel': True, 'YearsCode': True, 'ConvertedComp': True, 'JobSat': True, 'JobSeek': True, 'LanguageWorkedWith': True, 'DatabaseWorkedWith': True, 'WebFrameWorkedWith': True, 'SOVisitFreq': True, 'Age': True, 'Gender': True, "_id": False}
 
-@app.route("/", methods = ["GET"])
+
+# app.config['MONGO_URI'] = "mongodb://localhost:27017/stack_overflow"
+# mongo = PyMongo(app)
+
+
+@app.route("/", methods=["GET"])
 def index():
-    # return render_template("index.html")
+    return render_template("index.html")
+
+@app.route("/stack_overflow/developers_cleaned")
+def build_project():
     try:
-        developers = mongo.db.developers
-        result = developers.find({})
-        return render_template("index.html", result = result)
+        connection = MongoClient(MONGODB_HOST, MONGODB_PORT)
+        collection = connection[DATABASE_NAME][COLLECTION_NAME]
+        projects = collection.find(projection=FIELDS)
+        # developers = mongo.db.developers
+        # results = developers.find({})
+        json_projects = []
+        for project in projects:
+            json_projects.append(project)
+        json_projects = json.dumps(json_projects, default=json_util.default)
+        connection.close()
+        return json_projects
+        # return render_template("index.html", results = results)
         # return jsonify({"results": result['LanguageWorkedWith']})
     except Exception as e:
         return dumps({"error": str(e)})
+
 
 # @app.route("/data", methods = ["GET"])
 # def data():
---
+++ static/js/logic.js
@@ -0,0 +1,131 @@
+queue()
+    .defer(d3.json, '/stack_overflow/developers_cleaned')
+    .await(makeGraphs);
+
+function makeGraphs(error, projectsJson) {
+	var stack_overflow_repo = projectsJson;
+	var ndx = crossfilter(stack_overflow_repo);
+
+//    function reduceAdd(p, v) {
+//        v.tags.forEach (function(val, idx) {
+//        p[val] = (p[val] || 0) + 1; //increment counts
+//      });
+//      return p;
+//    }
+//
+//    function reduceRemove(p, v) {
+//        v.tags.forEach (function(val, idx) {
+//        p[val] = (p[val] || 0) - 1; //decrement counts
+//      });
+//      return p;
+//    }
+//
+//    function reduceInitial() {
+//        return {};
+//    }
+//
+//    var tags = ndx.dimension(function(d){ return d.LanguageWorkedWith;});
+//    var tagsGroup = tags.groupAll().reduce(reduceAdd, reduceRemove, reduceInitial).value();
+//    // hack to make dc.js charts work
+//    tagsGroup.all = function() {
+//        var newObject = [];
+//        for (var key in this) {
+//            if (this.hasOwnProperty(key) && key != "all") {
+//                newObject.push({
+//                key: key,
+//                value: this[key]
+//                });
+//            }
+//        }
+//      return newObject;
+//    }
+	
+	
+//	var pieTypeDim = ndx.dimension(function(d) { return d.AUTHORIZED_CAPITAL; });
+//	var pieDim = ndx.dimension(function(d) { return d.COMPANY_CLASS; });
+//	var rowLevelDim = ndx.dimension(function(d) { return d.PRINCIPAL_BUSINESS_ACTIVITY; });
+//	var bubbleDim = ndx.dimension(function(d) { return d.COMPANY_STATUS; });
+//	var DataDim = ndx.dimension(function(d) { return d.COMPANY_NAME; });
+    var country_dim = ndx.dimension(function(d) { return d.Country;});
+    var yearsCode_dim = ndx.dimension(function(d) { return d.YearsCode;});
+    var hobby_dim = ndx.dimension(function(d) { return d.Hobbyist;});
+    var jobSat_dim = ndx.dimension(function(d) { return d.JobSat;});
+    var jobSeek_dim = ndx.dimension(function(d) { return d.JobSeek;});
+    var language_dim = ndx.dimension(function(d) {return d.LanguageWorkedWith;});
+    var db_dim = ndx.dimension(function(d) {return d.DatabaseWorkedWith;});
+    var wbFrame_dim = ndx.dimension(function(d) {return d.WebFrameWorkedWith;});
+    var age_dim = ndx.dimension(function(d) {return d.Age;});
+    var gender_dim = ndx.dimension(function(d) {return d.Gender;});
+
+    console.log(country_dim);
+
+	var GroupByCountry = country_dim.group()
+	    .reduceSum(function (d) {return d.Country;})
+//	var numProjectsByResourceType = pieTypeDim.group()
+//	.reduceCount(function(d){return d.AUTHORIZED_CAPITAL;});
+//	var pieDimType = pieDim.group()
+//	.reduceCount(function(d){return d.COMPANY_CLASS;});
+//	var numProjectsByPovertyLevel = rowLevelDim.group()
+//	.reduceCount(function(d){return d.PRINCIPAL_BUSINESS_ACTIVITY;});
+//	var bubbleDimgroup = bubbleDim.group()
+//	.reduceCount(function(d){return d.COMPANY_NAME;});
+
+	
+	
+//	var DataDimgroup = DataDim.group()
+//	.reduceCount(function(d){return d.COMPANY_STATUS;});
+    
+//	var rowChart = dc.pieChart("#pie-chart");
+	var row1Chart = dc.rowChart("#row-chart");
+//	var bubbleChart = dc.rowChart("#bubble-chart");
+//	var dataTable = dc.dataTable("#data-chart");
+
+//	rowChart
+//        .width(400)
+//        .height(400)
+//		.dimension(pieTypeDim)
+//        .group(numProjectsByResourceType)
+//		.radius(200)
+//		.slicesCap(5)
+//    .legend(dc.legend().gap(3));
+       
+		
+//	dataTable
+//	.width(960)
+//	.height(800)
+//
+//    .dimension(DataDim)
+//    .group(function(d) { return d.COMPANY_NAME;
+//     })
+//
+//    .columns([
+//     function(d) { return d.COMPANY_STATUS; },
+//     function(d) { return d.COMPANY_CLASS; },
+//
+//	  function(d) { return d.PAIDUP_CAPITAL; },
+//      function(d) { return d.AUTHORIZED_CAPITAL; },
+//
+//      function(d) { return d.PRINCIPAL_BUSINESS_ACTIVITY; },
+//    ])
+//    .sortBy(function(d){ return d.PRINCIPAL_BUSINESS_ACTIVITY; })
+//    .order(d3.ascending);
+//
+//
+//	bubbleChart
+//         .width(700)
+//        .height(150)
+//		.dimension(pieDim)
+//        .group(pieDimType)
+//		.xAxis().tickFormat();
+		
+	row1Chart
+        .width(600)
+        .height(600)
+		.dimension(country_dim)
+        .group(GroupByCountry)
+		.xAxis().tickFormat(50);
+        	
+
+	
+    dc.renderAll();
+}
